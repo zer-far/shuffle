8c9e5f5d3f56a5e5df6f1a061bc041c015aad3e6
Abdullah <73560425+zer-far@users.noreply.github.com>
2021-10-06 19:20:23 +0000

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  const $random = $('.nbr');
+  const $timer = 30;
+  let $it;
+  let $data = 0;
+  let index;
+  let change;
+  let letters = ['p', 'a', 't', 't', 'e', 'r', 'n'];
+
+  $random.each(function () {
+    change = Math.round(Math.random() * 100);
+    $(this).attr('data-change', change);
+  });
+
+  function random() {
+    return Math.round(Math.random() * 9);
+  }
+
+  function select() {
+    return Math.round(Math.random() * $random.length + 1);
+  }
+
+  function value() {
+    $('.nbr:nth-child(' + select() + ')').html('' + random() + '');
+    $('.nbr:nth-child(' + select() + ')').attr('data-number', $data);
+    $data++;
+
+    $random.each(function () {
+      if (parseInt($(this).attr('data-number')) > parseInt($(this).attr('data-change'))) {
+        index = $('.ltr').index(this);
+        $(this).html(letters[index]);
+        $(this).removeClass('nbr');
+      }
+    });
+  }
+
+  $it = setInterval(value, $timer);
+});
